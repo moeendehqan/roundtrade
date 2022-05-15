3e81378862ff748487d4d737a6db0219d748a885
moeendehqan <moeen.dehqan@gmail.com>
2022-05-15 12:14:42 +0430

    desk 6

---
+++ src/components/desk/menur.js
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+
+const Menur = (props)=>{
+    const menumode = props.menumode
+    return(
+        <div>
+            kkkk
+            {menumode}
+        </div>
+    )
+}
+
+
+export default Menur
