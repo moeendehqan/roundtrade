5cccd396479506aa39170f675f59f5db535b125a
moeendehqan <moeen.dehqan@gmail.com>
2022-05-15 12:45:34 +0430

    desk20

--- src/components/desk/desk.js
+++ src/components/desk/desk.js
@@ -2,17 +2,19 @@ import './desk.css'
 import {useState} from 'react'
 import Menur from './menur'
 const Desk = () =>{
-    const [menumode, setMenumode] = useState(false)
-    const swochemenumode = ()=>{setMenumode(!menumode)}
+
+    const [munemode, setMenumode] = useState(false)
+    const sowchemunemode = ()=>{setMenumode(!munemode)}
+    console.log(munemode)
 
     return(
         <div>
             <nav className='dnav'>
-                
+            <button className='dmenu' onClick={(e)=>sowchemunemode()}><img src={require('./img/hmb.png')}></img></button>
                 <h3>رنـــد تــریـــد</h3>
                 <img src={require('../home/02.png')} alt='Logo RoundTrade'></img>
             </nav>
-            <Menur menumode={menumode} />
+            <Menur munemode={munemode} />
 
         </div>
     )
--- src/components/desk/menur.js
+++ src/components/desk/menur.js
@@ -1,12 +1,11 @@
 import {useState} from 'react'
 
 const Menur = (props)=>{
-    const menumode = props.menumode
 
     return(
         <div>
             <div className='menur'>
-            <button className='dmenu'><img src={require('./img/hmb.png')}></img></button>
+            
 
             </div>
             
